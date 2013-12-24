6942d0ad0ebb42aad8de8213d9ff575486d8b034
pbharadiya <parthbharadiya007@gmail.com>
2013-12-24 11:57:54 +0530

    Add precompiled assets for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ EventManagement::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true  #changed to true for appfog configuration
+  config.serve_static_assets = false  #changed to false for heroku configuration
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
